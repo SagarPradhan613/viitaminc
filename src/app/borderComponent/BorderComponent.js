aaabbd88acbebad22f2e0681986f39e89714c12b
SagarPradhan613 <karanpradhan42@gmail.com>
2023-10-10 10:43:21 +0530

    every ui fixed

--- src/app/borderComponent/BorderComponent.js
+++ src/app/borderComponent/BorderComponent.js
@@ -8,11 +8,17 @@ const BorderComponent = () => {
             <div class="lg:h-[30vh] h-[10vh] lg:hidden block bg-gradient-to-b from-[#e6af1e] to-white"></div>
             <div className='flex flex-col justify-center items-center align-middle vitamintext-container'>
                 <img src='Images/vitamintext.png' className='vitamintextimg w-2/3' ></img>
-                <div className='absolute bg-[#2F2F2F]  opacity-50 flex justify-center items-center align-middle left-0 responsive-side-logo'>
+                <div className='hidden absolute bg-[#2F2F2F]  opacity-50 md:flex justify-center items-center align-middle left-0 responsive-side-logo'>
                     <img src='Images/borderlogo.png' alt='Border Logo' className='opacity-1 bg-[lightgray] bg-center bg-cover mix-blend-luminosity w-full' />
                 </div>
-                <div className='absolute bg-[#2F2F2F]  opacity-50 flex justify-center items-center align-middle right-0 responsive-side-logo'>
+                <div className='hidden absolute bg-[#2F2F2F]  opacity-50 md:flex justify-center items-center align-middle right-0 responsive-side-logo'>
                     <img src='Images/borderlogo.png' alt='Border Logo' className='opacity-1 bg-[lightgray] bg-center bg-cover mix-blend-luminosity w-full' />
+                </div>
+                <div className='md:hidden absolute bg-[#2F2F2F]  opacity-50 flex justify-center items-center align-middle left-0 responsive-side-logo w-[50px]'>
+                    <img src='Images/borderlogo.png' alt='Border Logo' className='opacity-1 bg-[lightgray] bg-center bg-cover mix-blend-luminosity ' />
+                </div>
+                <div className='md:hidden absolute bg-[#2F2F2F]  opacity-50 flex justify-center items-center align-middle right-0 responsive-side-logo w-[50px]'>
+                    <img src='Images/borderlogo.png' alt='Border Logo' className='opacity-1 bg-[lightgray] bg-center bg-cover mix-blend-luminosity ' />
                 </div>
             </div>
             <div class="lg:h-[30vh] h-[10vh] lg:hidden block bg-gradient-to-t from-[#e6af1e] to-white"></div>
--- src/app/landingBorder/LandingBorder.js
+++ src/app/landingBorder/LandingBorder.js
@@ -7,11 +7,17 @@ const LandingBorder = () => {
             <div className='pt-2'>
                 <div className='flex flex-col justify-center items-center align-middle vitamintext-container relative'>
                     <img src='Images/vitamintext.png' className='vitamintextimg w-2/3' ></img>
-                    <div className='absolute bg-[#2F2F2F]  opacity-50 flex justify-center items-center align-middle left-0 responsive-side-logo'>
+                    <div className='hidden absolute bg-[#2F2F2F]  opacity-50 md:flex justify-center items-center align-middle left-0 responsive-side-logo'>
                         <img src='Images/borderlogo.png' alt='Border Logo' className='opacity-1 bg-[lightgray] bg-center bg-cover mix-blend-luminosity w-full' />
                     </div>
-                    <div className='absolute bg-[#2F2F2F]  opacity-50 flex justify-center items-center align-middle right-0 responsive-side-logo'>
+                    <div className='hidden absolute bg-[#2F2F2F]  opacity-50 md:flex justify-center items-center align-middle right-0 responsive-side-logo'>
                         <img src='Images/borderlogo.png' alt='Border Logo' className='opacity-1 bg-[lightgray] bg-center bg-cover mix-blend-luminosity w-full' />
+                    </div>
+                    <div className='md:hidden  absolute bg-[#2F2F2F]  opacity-50 flex justify-center items-center align-middle left-0 w-[50px]'>
+                        <img src='Images/borderlogo.png' alt='Border Logo' className='opacity-1 bg-[lightgray] bg-center bg-cover mix-blend-luminosity ' />
+                    </div>
+                    <div className='md:hidden absolute bg-[#2F2F2F]  opacity-50 flex justify-center items-center align-middle right-0  w-[50px]'>
+                        <img src='Images/borderlogo.png' alt='Border Logo' className='opacity-1 bg-[lightgray] bg-center bg-cover mix-blend-luminosity ' />
                     </div>
                 </div>
                 <div class="lg:h-[30vh] h-[10vh] bg-gradient-to-t from-[#fee173] to-white"></div>
